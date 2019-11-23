4f392109928a475c54d1b3e9d1614f4cfbd4f64c
Ben Rongey <benjamin.rongey@gmail.com>
2019-11-23 10:10:08 -0600

    finished processes

--- elixir/big_parse.ex
+++ elixir/big_parse.ex
@@ -1,8 +1,28 @@
-file = "big.txt"
+# The ultimate objective here is to take a large text file (potentially a wikipedia dump of filesize n), parse it and get
+# a count of words or wordlength.
+
+file = "test.txt"
+
+case File.read(file) do
+  {:ok, body} ->
+    body
+    |> IO.puts
+  {:error, reason} ->
+    "Could not read file #{file} because #{reason}"
+    |> IO.puts
+end
 
 case File.read(file) do
   {:ok, body} ->
     String.split(body)
+    |> length
+    |> IO.puts
+end
+
+
+
+case File.read(file) do
+    {:ok, body} -> String.split(body)
     |> length
     |> IO.puts
 end
